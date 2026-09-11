dc544d6991ce17f113f1c46c098fac9fcfdec1d6
Ajaylc7 <ronaldoajay80@gmail.com>
2026-09-11 12:11:59 +0530

    Update mount_uuid.sh

--- mount_uuid.sh
+++ mount_uuid.sh
@@ -2,27 +2,30 @@
 
 # ==========================================
 # Mount Filesystem Using UUID
-# Student Name:
-# Roll Number:
+# Student Name:AJAY T
+# Roll Number:1U24IT004
 # ==========================================
 
 
 # Display filesystem UUID
-
+sudo blkid
 
 
 
 # Create mount directory
-
+sudo mkdir -p /mnt/mydisk
 
 
 
 # Mount filesystem using UUID
+sudo blkid
 # Replace YOUR_UUID with actual UUID
+sudo mount UUID=YOUR_UUID /mnt/mydisk
 
 
 
 
 # Display mounted filesystem
+df -h /mnt/mydisk
 
 
